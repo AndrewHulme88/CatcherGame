1c5296d6869eb80648f5c678800ed22f3aa3a31f
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2024-11-15 18:08:30 +1100

    Add files via upload

--- EnemyBlob.cs
+++ EnemyBlob.cs
@@ -6,24 +6,34 @@ public class EnemyBlob : Enemy
 {
     [Header("Blob Specifics")]
     [SerializeField] private float dropSpeed = 5f;
-    private Transform enemyTransform;
-    private bool isDropping = false;
     private enum EnemyState { Ceiling, Dropping, Ground }
     private EnemyState enemyState = EnemyState.Ceiling;
     private Vector2 moveDirection = Vector2.right;
 
+    public bool playerFound = false;
+
     protected override void Start()
     {
-        enemyTransform = transform;
+        base.Start();
         rb.gravityScale = 0f;
     }
 
     void Update()
     {
+        CollisionChecks();
+
         switch(enemyState)
         {
             case EnemyState.Ceiling:
-                
+                MoveOnCeiling();
+                DetectPlayerBelow();
+                break;
+            case EnemyState.Dropping:
+                DropDown();
+                break;
+            case EnemyState.Ground:
+                Patrol();
+                break;
         }
     }
 
@@ -35,11 +45,40 @@ public class EnemyBlob : Enemy
         {
             idleTimeCounter = idleTime;
             Flip();
+            moveDirection = -moveDirection;
         }
     }
 
     private void DetectPlayerBelow()
     {
+        RaycastHit2D playerHit = Physics2D.Raycast(transform.position, Vector2.down, playerDetectDistance, playerMask);
 
+        if(playerHit.collider != null)
+        {
+            playerFound = true;
+            enemyState = EnemyState.Dropping;
+            transform.localScale = new Vector3(1, 1, 1);
+            groundCheckDistance = -groundCheckDistance;
+            rb.gravityScale = 1f;
+        }
     }
+
+    private void DropDown()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, -dropSpeed);
+
+        if(groundDetected)
+        {
+            enemyState = EnemyState.Ground;
+        }
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * playerDetectDistance);
+    }
+
 }
